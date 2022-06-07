3d4d7f85b0001974e3be6d67e25140b50e9d70bb
Aneta Grochowska <aneta.anette.g3@gmail.com>
2022-06-07 10:10:34 +0200

    shorter code

--- main.js
+++ main.js
@@ -1,34 +1,45 @@
 // console.log("Hello World");
 
-// A function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’.
+// A function called computerPlay that will randomly return either ‘Rock’, ‘Paper’ or ‘Scissors’. Case insensitive result
+let computerSelection;
 
 let options = ['Rock', 'Paper', 'Scissors'];
 
 function computerPlay() {
-    return options[Math.floor(Math.random() * options.length)];
+    return options[~~(Math.random() * options.length)].toUpperCase();
 }
 console.log(computerPlay());
 
-
 // Write a function that plays a single round of Rock Paper Scissors. The function should take two parameters - the playerSelection and computerSelection - and then return a string that declares the winner of the round like so: "You Lose! Paper beats Rock"
+let playerSelection = prompt("Select from rock, paper and scissors").toUpperCase;
 
-function oneRound(playerSelection, computerSelection) {
-    if (playerSelection === computerSelection) {
+
+function playRound(playerSelection, computerSelection) {
+    computerSelection = computerPlay().toUpperCase();
+    // playerSelection = playerSelection.toUpperCase();
+    if (playerSelection == computerSelection) {
         console.log('ISSSA DRAW!!');
-    } else if (playerSelection == 'Rock' && computerSelection == 'Paper') {
-        console.log('You won!');
-    } else if (playerSelection == 'Paper' && computerSelection == 'Rock') {
-        console.log('You lost!!!');
-    } else if (playerSelection == 'Scissors' && computerSelection == 'Paper') {
-        console.log('You won!');
-    } else if (playerSelection == 'Rock' && computerSelection == 'Scissors') {
-        console.log('You won!');
-    } else if (playerSelection == 'Paper' && computerSelection == 'Scissors') {
-        console.log('You lost!');
-    } else if (playerSelection == 'Scissors' && computerSelection == 'Rock') {
-        console.log('You lost!');
+    } else if (playerSelection == 'rock') {
+        if (computerSelection == 'paper') {
+            console.log('You lost!');
+        } else {
+            console.log('You win')
+        }
+    } else if (playerSelection == 'paper') {
+        if (computerSelection == 'scissors') {
+            console.log('You lose');
+        } else {
+            console.log('You win')
+        }
+    } else if (playerSelection == 'scissors') {
+        if (computerSelection == 'rock') {
+            console.log('You lose') 
+        } else {
+            console.log('You win');
+        }
+    } else {
+        console.log('ERROR');
     }
 }
 
-// Make your function’s playerSelection parameter case-insensitive (so users can input rock, ROCK, RocK or any other variation).
-// Important note: you want to return the results of this function call, not console.log() them. You’re going to use what you return later on, so let’s test this function by using console.log to see the results:
+console.log(playRound(playerSelection, computerSelection));
